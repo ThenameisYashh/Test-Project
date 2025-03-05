1605227ba6d20779fc3c7b56211215326015133a
Yash Swarnkar <nuttcrits@gmail.com>
2025-03-05 13:47:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu')
+const hamburgerwrppr = document.querySelector('.hamburgerwrppr')
+
+menu.addEventListener("click",()=>{
+    hamburgerwrppr.classList.toggle('hamburgeron')
+})
